6ca9d0fa1bebced20ffab1065929c9260a69a0e7
Jacques Foottit <jacques@dotdot.email>
2025-04-14 15:22:52 -0400

    init: service react lib

---
+++ dotdot/libs/services/midi-sync/react/README.md
@@ -0,0 +1,7 @@
+# services-midi-sync-react
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test services-midi-sync-react` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ dotdot/libs/services/midi-sync/react/src/lib/services-midi-sync-react.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledServicesMidiSyncReact = styled.div`
+  color: pink;
+`;
+export function ServicesMidiSyncReact() {
+  return (
+    <StyledServicesMidiSyncReact>
+      <h1>Welcome to ServicesMidiSyncReact!</h1>
+    </StyledServicesMidiSyncReact>
+  );
+}
+
+export default ServicesMidiSyncReact;
